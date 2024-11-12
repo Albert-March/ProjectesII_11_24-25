d85b8769aa2b8b8b5f53b91c5eec7e12d50adcca
Aberche <73166328+Aberche@users.noreply.github.com>
2024-11-12 15:13:27 +0100

    Implementation Virus1 Shoot

--- Insider/Assets/Project/Scripts/TowerRelated/AttackTypes/A_Virus1.cs
+++ Insider/Assets/Project/Scripts/TowerRelated/AttackTypes/A_Virus1.cs
@@ -1,15 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.UI;
 
 public class A_Virus1 : MonoBehaviour, IAttackType
 {
+    private Enemy target;
+    
+    private string assetAddress = "Prefabs/BulletLaser";
+    private GameObject laserPrefab;
+    private GameObject laser;
+    private Vector2 direction;
+    private float angle;
+    private float distance;
+
+    void Start()
+    {
+        Addressables.LoadAssetAsync<GameObject>(assetAddress).Completed += OnPrefabLoaded;
+    }
+
+    private void OnPrefabLoaded(AsyncOperationHandle<GameObject> handle)
+    {
+        if (handle.Status == AsyncOperationStatus.Succeeded)
+        {
+            laserPrefab = handle.Result;
+        }
+    }
     public void Attack(Enemy e)
     {
-        Vector2 direction = (e.transform.position - transform.position).normalized;
-        float distance = Vector2.Distance(transform.position, e.transform.position);
+        target = e;
+        direction = (target.transform.position - transform.position).normalized;
+        angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        distance = Vector2.Distance(transform.position, target.transform.position);
         RaycastHit2D fire = Physics2D.Raycast(transform.position, direction, distance, LayerMask.GetMask("Enemy"));
-        Debug.DrawRay(transform.position, e.transform.position - transform.position, Color.blue);
+        DrawLaser();
+        Debug.DrawRay(transform.position, target.transform.position - transform.position, Color.blue);
         Debug.Log(fire.transform.name);
         IDamage enemyDmg = fire.transform.GetComponent<IDamage>();
         if (enemyDmg != null)
@@ -17,4 +44,23 @@ public class A_Virus1 : MonoBehaviour, IAttackType
             enemyDmg.Damage(GetComponent<Tower>().damage);
         }
     }
+
+    void DrawLaser()
+    {
+        laser = Instantiate(laserPrefab, Vector3.Lerp(transform.position, target.transform.position, 0.5f), Quaternion.Euler(0, 0, angle));
+        laser.GetComponent<SpriteRenderer>().size = new Vector2(distance, laser.GetComponent<SpriteRenderer>().size.y);
+        StartCoroutine(DeleatLaser());
+    }
+
+    IEnumerator DeleatLaser()
+    {
+        while (laser.transform.localScale.y > 0) 
+        {
+            laser.transform.localScale = new Vector2(1, laser.transform.localScale.y - (1 *Time.deltaTime));
+            yield return null;
+        }
+        Destroy(laser);
+
+    }
 }
+
