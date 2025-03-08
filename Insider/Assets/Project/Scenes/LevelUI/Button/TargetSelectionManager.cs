f1848b2394c2e67cc9554b9eb0f8e5ebd4187875
Aberche <73166328+Aberche@users.noreply.github.com>
2025-03-08 20:54:43 +0100

    Build fixes

--- Insider/Assets/Project/Scenes/LevelUI/Button/TargetSelectionManager.cs
+++ Insider/Assets/Project/Scenes/LevelUI/Button/TargetSelectionManager.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -45,7 +45,7 @@ public class TargetSelectionManager : MonoBehaviour
         }
         dinamicPanel.GetComponent<SetTowerBaseInput>().clickedButton.gameObject.transform.GetChild(2).GetComponent<Tower>().targetType = t;
         currentTarget = target;
-        selectedTargetText.text = $"Target: {currentTarget}";
+        selectedTargetText.text = $" ↓ Target: {currentTarget}";
         targetOptionsPanel.SetActive(false);
     }
 }
