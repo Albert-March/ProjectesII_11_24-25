125a9523a28cffbf184ee5cb2f781f07527c2a09
Albert <albert.march@enti.cat>
2025-06-01 13:02:44 +0200

    DamageEffect

--- Insider/Assets/Project/Scripts/ParasiteRelated/ParasiteManager.cs
+++ Insider/Assets/Project/Scripts/ParasiteRelated/ParasiteManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
 using UnityEngine.Localization;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -17,11 +19,30 @@ public class ParasiteManager : MonoBehaviour, IDamage
 
 	public LocalizedString hpTable;
 
+	//DamageEffect
+	public float intensity = 0;
+	public GameObject postProcessObject;
+	private Volume _volume;
+	private Vignette _vignette;
+
 	private void Start()
 	{
 		lastHealth = parasiteHealth;
 		UpdateHPText();
 		originalScale = parasiteTextMoney.transform.localScale;
+
+		// Obtener el PostProcessVolume del GameObject
+		_volume = postProcessObject.GetComponent<Volume>();
+		if (_volume != null && _volume.profile.TryGet(out _vignette))
+		{
+			_vignette.active = false;
+		}
+		else
+		{
+			Debug.LogWarning("No se encontró el efecto Vignette en el perfil.");
+		}
+
+
 	}
 
 	private void Update()
@@ -30,6 +51,7 @@ public class ParasiteManager : MonoBehaviour, IDamage
 		{
 			scale = 1.5f;
 			lastHealth = parasiteHealth;
+			StartCoroutine(TakeDamageEffect());
 		}
 		UpdateHPText();
 
@@ -54,5 +76,30 @@ public class ParasiteManager : MonoBehaviour, IDamage
 	{
 		hpTable.Arguments = new object[] { parasiteHealth };
 		parasiteText.text = hpTable.GetLocalizedString();
+	}
+
+	private IEnumerator TakeDamageEffect()
+	{
+		intensity = 0.4f;
+
+		_vignette.active = true;
+		_vignette.intensity.Override(0.4f);
+
+		yield return new WaitForSeconds(0.4f);
+
+		while(intensity > 0)
+		{
+			intensity -= 0.01f;
+
+			if(intensity < 0) intensity = 0;
+
+			_vignette.intensity.Override(intensity);
+
+			yield return new WaitForSeconds(0.1f);
+		}
+
+		_vignette.active = false;
+
+		yield break;
 	}
 }
